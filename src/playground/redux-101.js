288de64218dd37ce3048c7c198cb9205f89502e3
William Jacoby <billyjacoby@zeus.local>
2017-10-19 16:37:22 -0400

    playing with redux part 2

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,44 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = { count: 0}) => {
-  return state;
+// sets the state, if no current state the default is used.
+const store = createStore((state = { count: 0}, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 });
 
+// getState returns the current state
+// console.log(store.getState());
+
+
+// Actions are objects that get sent to the store
+// eg increment, decrement, reset
+store.dispatch({
+  type: 'INCREMENT',
+} );
+
+store.dispatch({
+  type: 'INCREMENT',
+} );
+
+store.dispatch({
+  type: 'RESET',
+} );
+
+store.dispatch({
+  type: 'DECREMENT',
+} );
 console.log(store.getState());
