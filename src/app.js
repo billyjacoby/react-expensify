46eb88525fd0f874b9007a8c179ce1ea548361c1
Billy Jacoby <billyjacoby@gmail.com>
2017-11-04 11:16:45 -0400

    refactored all redux code and added to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+// Redux stuff
+import configureStore from './store/configure-store';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibileExpenses from './selectors/expenses';
 // style
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import AppRouter from "./routers/app-router"
 
+const store = configureStore();
+
+// addExpense water bill, gas bill
+// setTextFilter
+// getVisibileExpense
+
+store.dispatch(addExpense({description: "Water Bill"}));
+store.dispatch(addExpense({description: "Gas Bill"}));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibileExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
