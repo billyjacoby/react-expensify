bbdedd6ca542f008486c77b0a6100f01bf335a0b
Billy Jacoby <billyjacoby@gmail.com>
2017-11-09 16:52:19 -0500

    added form to edit-expense-page

--- src/components/edit-expense-page.js
+++ src/components/edit-expense-page.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import ExpenseForm from './expense-form';
+import {editExpense, removeExpense} from '../actions/expenses';
 
-export default (props) => {
-  return <div>Editing expense id {props.match.params.id}</div>
+const EditExpensePage =  (props) => {
+  // console.log(props.expense.description);
+  return (
+    <div>
+      {
+        props.expense ?
+        <div>
+          <ExpenseForm
+            expense={props.expense}
+            onSubmit={(expense) => {
+              props.dispatch(editExpense(props.expense.id, expense));
+              props.history.push('/');
+              // console.log('updated', expense);
+            }}
+          />
+          <button
+            onClick = {() => {
+              props.dispatch(removeExpense({id: props.expense.id}));
+              props.history.push('/');
+            }}
+          >Remove</button>
+        </div>
+        : "Invalid expense id."
+      }
+    </div>
+  );
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === ownProps.match.params.id)
+  }
+}
+
+export default connect(mapStateToProps)(EditExpensePage);
