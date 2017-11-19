08629162abd8472bd58d1a3f319f10010c52a46b
Billy Jacoby <billyjacoby@gmail.com>
2017-11-19 18:04:59 -0500

    fixed webpack config to add sourceMap: true to css-loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-// entry point -> output
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-  console.log("env: ", env);
   const isProduction = env === 'production';
-
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
@@ -20,14 +18,28 @@ module.exports = (env) => {
         exclude: /node_modules/
       },{
         test: /\.s?css$/,
-        use:[
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
