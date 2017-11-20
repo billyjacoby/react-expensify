9665b23a69984466ca5f1ea16b7209880722f941
Billy Jacoby <billyjacoby@gmail.com>
2017-11-19 21:57:21 -0500

    cleaned up jsx in expenses summary

--- src/components/expenses-summary.js
+++ src/components/expenses-summary.js
@@ -5,21 +5,24 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = (props) => (
-  <div>
-    {
-      props.expenseCount > 0 ?
-      <div>
-        <h3>Expenses Summary</h3>
-        <p>You are viewing {props.expenseCount} {props.expenseCount === 1 ? "expense" : "expenses"} totaling {numeral(props.expensesTotal / 100).format('$0,0.00')}</p>
-      </div> :
-      <div>
-        <p>No expenses found!</p>
-      </div>
-    }
+export const ExpensesSummary = (props) =>{
+  const expenseWord = props.expenseCount === 1 ? "expense" : "expenses";
+  return (
+    <div>
+      {
+        props.expenseCount > 0 ?
+        <div>
+          <h3>Expenses Summary</h3>
+          <p>You are viewing {props.expenseCount} {expenseWord} totaling {numeral(props.expensesTotal / 100).format('$0,0.00')}</p>
+        </div> :
+        <div>
+          <p>No expenses found!</p>
+        </div>
+      }
 
-  </div>
-);
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => ({
   expenseCount: selectExpenses(state.expenses, state.filters).length,
